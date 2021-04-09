1c3ea79abdebef5c26d5659d59d5599e7764de1f
Yoshitsugu-Antony-Kambara <tsugu1999213@yahoo.co.jp>
2021-04-09 16:51:00 +0900

    ファイル整理

---
+++ Mpple/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  Mpple
+//
+//  Created by 神原良継 on 2021/03/27.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
